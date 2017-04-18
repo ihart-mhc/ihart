83f6573e69fc0785185a5ca8f02d9394ccc7f979
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-18 15:29:43 -0400

    set the position and size of a window; put on the top of others

--- server/src/server.py
+++ server/src/server.py
@@ -83,6 +83,8 @@ class Server:
         # self.frame_width = int(self.default_width * self.ratio)
         # self.frame_height = int(self.default_height * self.ratio)
         # self.root.geometry("%dx%d" % (self.frame_width, self.frame_height))
+        root.attributes("-topmost", True)
+        root.geometry("150x150+30+30")
 
         self.startButton = Button(root, text="Start", command=self.start)
         self.quitButton = Button(root, text="Quit")
